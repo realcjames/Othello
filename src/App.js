471f3a16972f5d7e382f05f84561da50be5a205e
realcjames <realcjames@163.com>
2019-07-18 17:19:42 +0800

    棋盘+轮流下子功能实现

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import update from "immutability-helper";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const whiteTurn = Symbol("white");
+const blackTurn = Symbol("black");
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chessPieces: Array(8)
+        .fill()
+        .map((n, i) =>
+          Array(8)
+            .fill()
+            .map((n2, i2) => ({
+              row: i + 1,
+              col: i2 + 1,
+              isOccupiedByBlack: false,
+              isOccupiedByWhite: false
+            }))
+        ),
+      turn: blackTurn
+    };
+  }
+
+  placeChess = (chess, i, j) => {
+    if (chess.isOccupiedByBlack || chess.isOccupiedByWhite) {
+      return;
+    }
+
+    const { turn } = this.state;
+    if (turn === whiteTurn) {
+      this.setState(prevState => ({
+        chessPieces: update(prevState.chessPieces, {
+          [i]: {
+            [j]: { isOccupiedByWhite: { $set: true } }
+          }
+        }),
+        turn: blackTurn
+      }));
+    }
+
+    if (turn === blackTurn) {
+      this.setState(prevState => ({
+        chessPieces: update(prevState.chessPieces, {
+          [i]: {
+            [j]: { isOccupiedByBlack: { $set: true } }
+          }
+        }),
+        turn: whiteTurn
+      }));
+    }
+  };
+
+  render() {
+    const { chessPieces, turn } = this.state;
+
+    return (
+      <div className="container">
+        {chessPieces.map((item, i) => (
+          <div className="row">
+            {item.map((chess, j) => (
+              <div
+                className="chess-item"
+                onClick={() => this.placeChess(chess, i, j)}
+              >
+                {chess.isOccupiedByBlack && (
+                  <img src={require("./assets/images/black.png")} alt="" />
+                )}
+                {chess.isOccupiedByWhite && (
+                  <img src={require("./assets/images/white.png")} alt="" />
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
